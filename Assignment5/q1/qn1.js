a524181ed8162e0e63617f5dbd56d007c62734a0
VeryLazyBoy <frank16874@gmail.com>
2019-02-03 23:02:47 +0800

    Fixes trend 1: cleans up the qn1 code

--- Assignment5/q1/qn1.js
+++ Assignment5/q1/qn1.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
 
     // Defines format function
-    if(!String.format) {
+    if (!String.format) {
         String.format = function(format) {
-          var args = Array.prototype.slice.call(arguments, 1);
-          return format.replace(/{(\d+)}/g, function(match, number) {
-            return typeof args[number] != 'undefined'
-              ? args[number]
-              : match
-            ;
-          });
+            var args = Array.prototype.slice.call(arguments, 1);
+            return format.replace(/{(\d+)}/g, function(match, number) {
+                return typeof args[number] != 'undefined' ?
+                    args[number] :
+                    match;
+            });
         };
     }
 
@@ -21,17 +20,17 @@ $(document).ready(function() {
             var input = $(this).get(0).value;
             var url = 'http://localhost:8080/json/places';
 
-            if(input.length < minLength)
+            if (input.length < minLength)
                 return false;
 
-            if(searchRequest != null)
+            if (searchRequest != null)
                 searchRequest.abort();
 
             searchRequest = $.getJSON(url, {
                 keyword: input,
                 limit: 8
             }, function(data) {
-                if(input == inputBar.val()) {
+                if (input == inputBar.val()) {
                     var places = '';
                     $('.confOptions').get(0).remove();
                     $.each(data.places, function(index, place) {
@@ -50,20 +49,22 @@ $(document).ready(function() {
     $('#conferenceInput').keyup(getKeyupHandler('#conferenceInputContainer'));
     $('#confListInput1').keyup(getKeyupHandler('#ConferenceListDiv1'));
 
-    var unnestDataGroup = function(data, children){
+    var unnestDataGroup = function(data, children) {
         var out = [];
-        data.forEach(function(d, i){
-        // console.log(i, d);
-        d_keys = Object.keys(d);
-        // console.log(i, d_keys)
-        values = d[children];
+        data.forEach(function(d, i) {
+            // console.log(i, d);
+            d_keys = Object.keys(d);
+            // console.log(i, d_keys)
+            values = d[children];
 
-        values.forEach(function(v){
-            d_keys.forEach(function(k){
-            if (k != children) { v[k] = d[k]}
+            values.forEach(function(v) {
+                d_keys.forEach(function(k) {
+                    if (k != children) {
+                        v[k] = d[k]
+                    }
+                })
+                out.push(v);
             })
-            out.push(v);
-        })
 
         })
         return out;
@@ -79,7 +80,7 @@ $(document).ready(function() {
             url: urlString
         }).then(function(dataGroup) {
             // alert("RESULTS LOADED!");
-            if(dataGroup.length == 0){
+            if (dataGroup.length == 0) {
                 alert("No Results!");
                 return false;
             }
@@ -94,23 +95,23 @@ $(document).ready(function() {
             var vis = d3.select("#custom-chart");
 
             var WIDTH = 1000,
-            HEIGHT = 500,
-            MARGINS = {
-                top: 50,
-                right: 20,
-                bottom: 50,
-                left: 50
-            };
+                HEIGHT = 500,
+                MARGINS = {
+                    top: 50,
+                    right: 20,
+                    bottom: 50,
+                    left: 50
+                };
 
-            lSpace = WIDTH/dataGroup.length;
+            lSpace = WIDTH / dataGroup.length;
             xScale = d3.scale.ordinal()
-            .rangePoints([MARGINS.left, WIDTH - MARGINS.right])
-            .domain(data.sort(function(a,b) {
-                return (a[dataKeys[0]] > b[dataKeys[0]]) ? 1 : ((b[dataKeys[0]] > a[dataKeys[0]]) ? -1 : 0);
-            }).map(function(d) {
-                // console.log(d[dataKeys[0]]);
-                return d[dataKeys[0]];
-            }));
+                .rangePoints([MARGINS.left, WIDTH - MARGINS.right])
+                .domain(data.sort(function(a, b) {
+                    return (a[dataKeys[0]] > b[dataKeys[0]]) ? 1 : ((b[dataKeys[0]] > a[dataKeys[0]]) ? -1 : 0);
+                }).map(function(d) {
+                    // console.log(d[dataKeys[0]]);
+                    return d[dataKeys[0]];
+                }));
 
             var minCount = d3.min(data, function(d) {
                 return d[dataKeys[1]];
@@ -127,11 +128,11 @@ $(document).ready(function() {
             }
 
             xAxis = d3.svg.axis()
-            .scale(xScale),
-            // .tickFormat(xAxisFormat),
-            yAxis = d3.svg.axis()
-            .scale(yScale)
-            .orient("left");
+                .scale(xScale),
+                // .tickFormat(xAxisFormat),
+                yAxis = d3.svg.axis()
+                .scale(yScale)
+                .orient("left");
 
             vis.append("svg:g")
                 .attr("class", "x axis")
@@ -159,28 +160,28 @@ $(document).ready(function() {
                 return d + moveToLeft + firstHalfCircle + secondHalfCircle;
             }
 
-            dataGroup.forEach(function(d,i) {
+            dataGroup.forEach(function(d, i) {
                 // console.log("d is " + Object.keys(d));
-                if(d[dataGroupKeys[1]].length == 1) {
+                if (d[dataGroupKeys[1]].length == 1) {
                     path = pathToCircle(lineGen(d[dataGroupKeys[1]]));
                 } else {
                     path = lineGen(d[dataGroupKeys[1]]);
                 }
                 vis.append('svg:path')
-                .attr('d', path)
-                .attr('stroke', function(d,j) {
+                    .attr('d', path)
+                    .attr('stroke', function(d, j) {
                         return "hsl(" + Math.random() * 360 + ",100%,50%)";
-                })
-                .attr('stroke-width', 2)
-                .attr('id', 'line_'+d[dataGroupKeys[0]])
-                .attr('fill', 'none');
+                    })
+                    .attr('stroke-width', 2)
+                    .attr('id', 'line_' + d[dataGroupKeys[0]])
+                    .attr('fill', 'none');
                 vis.append("text")
-                    .attr("x", (lSpace/2)+i*lSpace)
+                    .attr("x", (lSpace / 2) + i * lSpace)
                     .attr("y", HEIGHT)
                     .style("fill", d3.select("#line_" + d[dataGroupKeys[0]]).attr('stroke'))
-                    .attr("class","legend")
-                    .on('click',function(){
-                        var active   = d.active ? false : true;
+                    .attr("class", "legend")
+                    .on('click', function() {
+                        var active = d.active ? false : true;
                         var opacity = active ? 0 : 1;
                         d3.select("#line_" + d[dataGroupKeys[0]]).style("opacity", opacity);
                         d.active = active;
@@ -196,22 +197,22 @@ $(document).ready(function() {
 
     var conferenceCounter = 2;
 
-    $("#addButton").click(function () {
-        if(counter>10){
+    $("#addButton").click(function() {
+        if (counter > 10) {
             alert("Max. of 10 years allowed.");
             return false;
         }
         var newTextBoxDiv = $(document.createElement('div'))
-        .attr("id", 'ConferenceYearDiv' + counter);
-        newTextBoxDiv.after().html('<label>Year '+ counter + ':</label>' +
-        '<input class="form-control" type="text" ' + counter +
-        '" id="confYearInput' + counter + '" placeholder="e.g. '+"'1993'" +'">');
+            .attr("id", 'ConferenceYearDiv' + counter);
+        newTextBoxDiv.after().html('<label>Year ' + counter + ':</label>' +
+            '<input class="form-control" type="text" ' + counter +
+            '" id="confYearInput' + counter + '" placeholder="e.g. ' + "'1993'" + '">');
         newTextBoxDiv.appendTo("#ConferenceYearsGroup");
         counter++;
     });
 
-    $("#removeButton").click(function () {
-        if(counter==2){
+    $("#removeButton").click(function() {
+        if (counter == 2) {
             alert("At least one year is required.");
             return false;
         }
@@ -219,17 +220,17 @@ $(document).ready(function() {
         $("#ConferenceYearDiv" + counter).remove();
     });
 
-    $("#addConferenceButton").click(function () {
-        if(conferenceCounter>10){
+    $("#addConferenceButton").click(function() {
+        if (conferenceCounter > 10) {
             alert("Max. of 10 Conferences allowed.");
             return false;
         }
         var newTextBoxDiv = $(document.createElement('div'))
-        .attr("id", 'ConferenceListDiv' + conferenceCounter).addClass('dropdown-content');
+            .attr("id", 'ConferenceListDiv' + conferenceCounter).addClass('dropdown-content');
 
-        newTextBoxDiv.after().html('<label>Conference '+ conferenceCounter + ':</label>' +
-        '<input class="form-control" type="text" ' + conferenceCounter +
-        '" id="confListInput' + conferenceCounter + '" placeholder="e.g. '+"'arXiv'" +'">');
+        newTextBoxDiv.after().html('<label>Conference ' + conferenceCounter + ':</label>' +
+            '<input class="form-control" type="text" ' + conferenceCounter +
+            '" id="confListInput' + conferenceCounter + '" placeholder="e.g. ' + "'arXiv'" + '">');
 
         newTextBoxDiv.appendTo("#ConferenceListGroup");
 
@@ -237,8 +238,8 @@ $(document).ready(function() {
         conferenceCounter++;
     });
 
-    $("#removeConferenceButton").click(function () {
-        if(conferenceCounter==2){
+    $("#removeConferenceButton").click(function() {
+        if (conferenceCounter == 2) {
             alert("At least one conference is required.");
             return false;
         }
@@ -252,26 +253,26 @@ $(document).ready(function() {
 
         var val = this.value;
         // console.log(val);
-        switch(val){
+        switch (val) {
             case '0': //over years
-            confYears = true;
-            confList = false;
-            break;
+                confYears = true;
+                confList = false;
+                break;
             case '1':
-            confYears = false;
-            confList = true;
-            break;
+                confYears = false;
+                confList = true;
+                break;
         }
-        if(confYears){
+        if (confYears) {
             $("#startYearInputContainer").removeClass("hidden");
             $("#endYearInputContainer").removeClass("hidden");
-        }else{
+        } else {
             $("#startYearInputContainer").addClass("hidden");
             $("#endYearInputContainer").addClass("hidden");
         }
-        if(confList){
+        if (confList) {
             $("#ConferenceListGroup").removeClass("hidden");
-        }else{
+        } else {
             $("#ConferenceListGroup").addClass("hidden");
         }
     });
@@ -281,13 +282,13 @@ $(document).ready(function() {
     };
 
     var isValidYear = function(yearString) {
-        if(yearString == ""){
+        if (yearString == "") {
             return true;
         }
-        if(!isNumeric(yearString)){
+        if (!isNumeric(yearString)) {
             return false;
         }
-        if(parseInt(yearString) < 2017 && parseInt(yearString) > 0){
+        if (parseInt(yearString) < 2017 && parseInt(yearString) > 0) {
             return true;
         }
         return false;
@@ -295,9 +296,9 @@ $(document).ready(function() {
 
     var getConferenceYears = function(yearCounter) {
         var yearList = [];
-        for(var t = 1; t <= yearCounter; t++) {
+        for (var t = 1; t <= yearCounter; t++) {
             var yearString = $('#confYearInput' + t).val();
-            if(yearString) {
+            if (yearString) {
                 //paseInst null or '' => NaN
                 yearList.push(parseInt(yearString));
             }
@@ -307,51 +308,51 @@ $(document).ready(function() {
 
     var getConferences = function(NumOfConfs) {
         var confList = [];
-        for(var t = 1; t <= NumOfConfs; t++) {
+        for (var t = 1; t <= NumOfConfs; t++) {
             var conf = $('#confYearInput' + t).val();
-            if(conf) {
+            if (conf) {
                 confList.push(conf);
             }
         }
         return confList;
     }
 
-    $("#generateBtn").click(function () {
+    $("#generateBtn").click(function() {
 
         var urlString;
-        if($('#queryTypeSelect').val() == 0){
+        if ($('#queryTypeSelect').val() == 0) {
             urlString = apiRootUrlOverYears;
 
             // Validates conference
             var conference = $("#conferenceInput").val();
-            if(!conference){
+            if (!conference) {
                 alert("Conference Code is required.");
                 return false;
             }
 
             // Valides start year
             var startYear = $("#startYearInput").val();
-            if(!isValidYear(startYear)){
+            if (!isValidYear(startYear)) {
                 alert("Start year is invalid");
                 return false;
             }
 
             // Validate end year
             var endYear = $("#endYearInput").val();
-            if(!isValidYear(endYear)){
+            if (!isValidYear(endYear)) {
                 alert("End year is invalid");
                 return false;
             }
 
             // Validate start year and end year
-            if(parseInt(startYear) > parseInt(endYear)){
+            if (parseInt(startYear) > parseInt(endYear)) {
                 alert("Start year must be before end year.");
                 return false;
             }
 
             // Validates conf year list
             var yearList = getConferenceYears(counter);
-            if(yearList.length == 0){
+            if (yearList.length == 0) {
                 alert("At least 1 conference year is required.");
                 return false;
             }
@@ -360,13 +361,13 @@ $(document).ready(function() {
             // End year should be chosen meaningfully to avoid all 0 counts.
             yearList.sort((a, b) => b - a);
             var maxConferenceYear = yearList[0];
-            if(maxConferenceYear < endYear) {
+            if (maxConferenceYear < endYear) {
                 alert("Plear choose end year that are not larger than " + maxConferenceYear + " .");
                 return false;
             }
 
             // Creates the final url
-            if(conference && conferenceYears){
+            if (conference && conferenceYears) {
                 urlString += "conf=" + conference + "&";
                 urlString += "years=" + conferenceYears + "&";
                 urlString += "syear=" + startYear + "&";
@@ -377,14 +378,14 @@ $(document).ready(function() {
 
             // Validates conference
             var conference = $("#conferenceInput").val();
-            if(!conference){
+            if (!conference) {
                 alert("Conference Code is required.");
                 return false;
             }
 
             // Validates conferenece year list
             var yearList = getConferenceYears(counter);
-            if(yearList.length == 0){
+            if (yearList.length == 0) {
                 alert("At least 1 conference year is required.");
                 return false;
             }
