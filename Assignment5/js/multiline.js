c29bc1871995792df3df820d38d5fcea4b0fefd4
VeryLazyBoy <frank16874@gmail.com>
2019-02-07 23:31:22 +0800

    Extracts String.formant into multiline.js

--- Assignment5/js/multiline.js
+++ Assignment5/js/multiline.js
@@ -1,6 +1,16 @@
 (function($) {
     var undefined;
-    console.log("gg");
+    // Defines format function
+    if (!String.format) {
+        String.format = function(format) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            return format.replace(/{(\d+)}/g, function(match, number) {
+                return typeof args[number] != 'undefined' ?
+                    args[number] :
+                    match;
+            });
+        };
+    }
 
     $.MultiLine = function(options) {
         var self = this;
@@ -168,5 +178,4 @@
         });
     };
 
-    console.log("ggg");
 })(jQuery);
--- Assignment5/q1/qn1.js
+++ Assignment5/q1/qn1.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
 
-    // Defines format function
-    if (!String.format) {
-        String.format = function(format) {
-            var args = Array.prototype.slice.call(arguments, 1);
-            return format.replace(/{(\d+)}/g, function(match, number) {
-                return typeof args[number] != 'undefined' ?
-                    args[number] :
-                    match;
-            });
-        };
-    }
-
     var searchRequest = null;
     var getKeyupHandler = function(container) {
         return function() {
